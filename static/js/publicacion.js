cfa416dae8ec827684611fd6ba38e0e5221498a6
Roberto Ramirez <robert.624@hotmail.com>
2024-04-25 16:35:14 -0500

    refactor: adding a modal for confirming comment deletion

--- static/js/publicacion.js
+++ static/js/publicacion.js
@@ -1,1 +1,40 @@
-console.log('publicacion.js loaded')
+const main = () => {
+
+    // delete example url "/eliminarcomment/{{id_img}}/{{comentario['id']}}"
+    const deleteUrl = "/eliminarcomment/"
+
+    const deleteCommentModal = document.getElementById('delete-comment-modal')
+
+    const deleteCommentTriggers = document.querySelectorAll('.delete-comment-trigger')
+
+    console.log("deleteCommentTriggers", deleteCommentTriggers)
+
+    deleteCommentTriggers.forEach(trigger => {
+        trigger.addEventListener('click', () => {
+            const commentId = trigger.getAttribute('data-comment-id')
+            const imageId = trigger.getAttribute('data-image-id')
+            const confirmDeleteButton = deleteCommentModal.querySelector('a.confirm-delete-comment')
+            const cancelDeleteButton = deleteCommentModal.querySelector('.cancel-delete-comment')
+
+            confirmDeleteButton.href = `${deleteUrl}${imageId}/${commentId}`
+            deleteCommentModal.classList.add('is-active')
+
+            cancelDeleteButton.addEventListener('click', () => {
+                deleteCommentModal.close()
+            })
+
+            // open modal
+            deleteCommentModal.showModal()
+
+            // close modal when clicking outside
+            deleteCommentModal.addEventListener('click', (e) => {
+                if (e.target === deleteCommentModal) {
+                    deleteCommentModal.close()
+                }
+            })
+        })
+    })
+
+}
+
+document.addEventListener('DOMContentLoaded', main)
